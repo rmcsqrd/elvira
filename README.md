5eb914340a73e7dfa87e27586123353a7df4cabf
elvira <rmcsqrd@gmail.com>
2020-04-17 12:53:06 -0600

    new commit after reinstall of os on jetson

---
+++ README.md
@@ -0,0 +1,5 @@
+## Elvira V
+
+This repository is the brain behind Elvira V. I'll update this readme later but this directory has my ros stuff etc. I think I'll probably break out the motor driver code on the arduino into a separate repo.   
+
+<a href="https://riomcmahon.me/portfolio/elvira/">Less garbled write up here</a>
---
+++ elvira_ros/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/elvira/programming/elvira/elvira_ros/src
+# Build directory: /home/elvira/programming/elvira/elvira_ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("realsense-ros/realsense2_description")
+subdirs("ddynamic_reconfigure")
+subdirs("realsense-ros/realsense2_camera")
